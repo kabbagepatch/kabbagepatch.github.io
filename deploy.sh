45f08fff1e0bc94a3bb5766bb78adc33f8c47b61
Kavish <kavishrmunjal@gmail.com>
2025-06-23 00:31:32 -0400

    Create node.js.yml (#35)
    * Create node.js.yml
    
    * Update node.js.yml
    
    * Update node.js.yml
    
    * Update node.js.yml
    
    * Update node.js.yml
    
    * Update node.js.yml
    
    * Update node.js.yml
    
    * Update node.js.yml
    
    * Update node.js.yml
    
    * Update deploy.sh
    
    * Update deploy.sh
    
    * Update deploy.sh
    
    * Update deploy.sh
    
    * Update node.js.yml
    
    * Update deploy.sh
    
    * Update node.js.yml
    
    * Update node.js.yml

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:kabbagepatch/kabbagepatch.github.io.git master
+git push -f https://kabbagepatch:$GIT_TOKEN@github.com/kabbagepatch/kabbagepatch.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
